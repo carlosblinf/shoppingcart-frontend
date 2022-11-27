ec627aac18385963fe572f38ea1208da6e5f550a
Carlos Ernesto Brooks Lorie <carlosblinf@gmail.com>
2022-11-27 11:56:50 -0500

    simple layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Store from './pages/Store'
+import './styles.scss'
 
 function App() {
 
   return (
-   <h1>Shopping Cart</h1>
+   <div className="containerApp">
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/store' element={<Store/>} />
+    </Routes>
+   </div>
   )
 }
 
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+    <main>
+      <div className="container">
+        <div className="product">
+          <img src="" alt="" />
+          <h3>product</h3>
+        </div>
+        <div className="product">
+          <img src="" alt="" />
+          <h3>product</h3>
+        </div>
+        <div className="product">
+          <img src="" alt="" />
+          <h3>product</h3>
+        </div>
+      </div>
+    </main>
+    </>
+  )
+}
+
+export default Home
